3a98195025f3b3375f08803184f613f2886add8f
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 19:15:23 +0530

    Create Type Conversion.java

---
+++ Practice Challenges/Type Conversion.java
@@ -0,0 +1,24 @@
+Given a double value d, typecast it to an integer value.
+
+Example 1:
+
+Input:
+d = 10.23
+Output:
+10
+Explanation:
+The integer value of 10.23 is 10
+ 
+
+Your Task:
+
+Your task is to complete the function typeCast() which converts the double value to integer and returns an integer. 
+ 
+Solution:
+public static int typeCast(double d){
+    // code here
+    int ans=(int)d;
+    return ans;
+    
+    
+}
