84854cd0ec0a1abdf48e440ac56c3caa1d688114
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 20:21:46 +0530

    Create Print Without Newline.java

---
+++ Practice Challenges/Print Without Newline.java
@@ -0,0 +1,27 @@
+As we have seen, the System.out.println() statement automatically appends a new line to our output. Here, we'll learn how to prevent that.
+You are given two string variables a and b, and you have to print a and b with a space between them. However, you must prevent the print statement from providing a new line as the new line will be given by the main driver code.
+
+Example 1:
+
+Input:
+a = "Hello"
+b = "World"
+Output: 
+Hello World
+Explanation: a and b are printed in a
+single line and a space separates
+them.The new line is provided by the
+driver code.
+
+Your Task: 
+Just write the syntax to print a and b in a single line with a space in between them while preventing print from providing a new line as the new line is auto appended by the driver code.
+ 
+Solution:
+public static void utility(String a, String b){
+     
+    //Write your code below. 
+
+
+    //Write your code below.
+    System.out.print(a+" "+b);
+}
