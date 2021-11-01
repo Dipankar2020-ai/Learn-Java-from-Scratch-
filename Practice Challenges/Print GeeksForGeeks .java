74dc31418235fec746c91b7194c83c970daf1d45
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 19:22:52 +0530

    Create Print GeeksForGeeks .java

---
+++ Practice Challenges/Print GeeksForGeeks .java
@@ -0,0 +1,25 @@
+You are given a print() function that outputs two statements one is "Hello World" and another "GeeksForGeeks". Comment the code that outputs "Hello World", so that "GeeksForGeeks" gets printed.
+
+Example 1: 
+
+Input:
+not any
+Output:
+GeeksForGeeks
+
+User Task:
+Your task is to comment the unnecessary statement.
+Solution:
+class Geeks{
+    
+    //comment the line that outputs "Hello World"
+    static void print(){
+        
+        //Prints Hello World
+       // System.out.println("Hello World");
+        //Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+      
+    }
+    
+}
