6058ab63ab06ce3c30f2e2ef4e74430cebf1c6a8
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 16:52:14 +0530

    Create output format.java

---
+++ Practice Challenges/output format.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int x=100;
+		int y=200;
+		double z=Math.PI;
+		System.out.format("The value of z=%.2f\n",z);
+		System.out.format("The value of z=%5.2f\n",z);
+		System.out.format("The value of z=%05.2f",z);
+		
+	}
+}
