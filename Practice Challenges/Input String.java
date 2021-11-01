8af4f5e15200e77d30c813740ecfff36123f8a37
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 21:15:37 +0530

    Create  Input String.java

---
+++ Practice Challenges/Input String.java
@@ -0,0 +1,18 @@
+Given a text in the form of a string, you need to input it. The text will then be printed.
+
+Example 1:
+
+Input:
+myInput = abcd
+Output: 
+abcd
+Explanation: The input is printed as it is.
+
+Your Task: 
+Just write the syntax to take an input at the mentioned place. The output code is already provided.
+Solution:
+String myInput =read.readLine();
+        
+//Complete the below code
+    
+System.out.println(myInput);
