0b63a8e13f0e27b29b637d1f0e902db17ebdb346
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 19:12:02 +0530

    Create Concatenate Integers.java

---
+++ Practice Challenges/Concatenate Integers.java
@@ -0,0 +1,9 @@
+Question:
+https://practice.geeksforgeeks.org/problems/concatenate-integers/1/?track=Java-Foundation-Data-Types&batchId=297#
+Solution:
+public  static  void concatenate(int a, int b){
+    //Complete the code below to concatenate a and b
+    String ans =String.valueOf(a)+String.valueOf(b);
+    //Complete the code above to concatenate a and b
+    System.out.println(ans);
+}
