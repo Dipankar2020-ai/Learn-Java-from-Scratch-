e796b7cf7b0b64db4c780aefbf530fe6e89c62a9
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 20:19:26 +0530

    Create Print With Space.java

---
+++ Practice Challenges/Print With Space.java
@@ -0,0 +1,26 @@
+Given two inputs that are store in variables a and b, you need to print a and b in a single line with a space separating them.
+
+Example 1:
+
+Input:
+a = "Hello"
+b = "World"
+Output: 
+Hello World
+Explanation: a and b are printed in a
+single line and a space separates them.
+
+Your Task: 
+Just write the syntax to print a and b in a single line with a space in between them. The input code is already provided
+
+Solution:
+public static void utility(String a, String b){
+
+    //Write your code below that prints a <space> b
+    
+    
+    
+    //Write your code below that prints a <space> b
+    System.out.println(a+" "+b);
+
+}
