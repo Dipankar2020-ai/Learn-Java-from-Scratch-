d2b86c58bed06a912146e4aa053fd9273b767cf6
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 20:24:43 +0530

    Create Print With Separator.java

---
+++ Practice Challenges/Print With Separator.java
@@ -0,0 +1,23 @@
+You'll be given two strings a and b, a separator symbol, and you need to print a and b such that a and b are separated by the separator symbol.
+
+Example 1:
+
+Input:
+a = "Hello"
+b = "World"
+separator = "@"
+Output: 
+Hello@World
+Explanation: a and b are printed
+with the separator symbol in between.
+
+Your Task: 
+Just write the syntax to print a and b in a single line with the separator symbol in between them. The input has already been provided.
+  
+ Solution:
+public static void utility(String a, String b, String separator){
+
+    //Your code below
+    System.out.println(a+separator+b);
+
+}
