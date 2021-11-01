fa9316dc53f76543bf681f0d98cb2f0bc6c76eb6
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 21:21:32 +0530

    Create  Input Integer.java

---
+++ Practice Challenges/Input Integer.java
@@ -0,0 +1,18 @@
+Given an integer, you need to input it. The integer will then be printed after adding 10.
+
+Example 1:
+
+Input:
+myInput = 2
+Output: 
+12
+Explanation: The input is 2, the print
+statement adds 10 to it to make 12.
+
+Your Task: 
+Just write the syntax to take an input at the mentioned place. The output code is already provided.
+  
+  Solution:
+int myInput=scn.nextInt();
+//Complete the code
+System.out.println(myInput + 10);
