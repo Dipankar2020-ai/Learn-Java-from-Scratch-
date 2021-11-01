4340d83b349006dd11f580530ce76da64fceb5d2
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 21:23:50 +0530

    Create Input Floating Number .java

---
+++ Practice Challenges/Input Floating Number .java
@@ -0,0 +1,20 @@
+Given a floating number, you need to input it. The floating number will then be printed after multiplying it by 10.
+
+Example 1:
+
+Input:
+myInput = 2.23
+Output: 
+22.3
+Explanation: The input is 2.23, the
+multiplication by 10 yields 2.23, it
+then gets printed.
+
+Your Task: 
+Just write the syntax to take an input at the mentioned place. The output code is already provided.
+  
+ Solution:
+//Complete the code
+double myInput=scn.nextDouble();
+//Complete the code
+System.out.println(myInput * 10);
