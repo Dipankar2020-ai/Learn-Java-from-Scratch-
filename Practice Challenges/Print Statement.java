c9bc7be97c05d4fa80cde190fb584a501fc81283
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-01 20:17:38 +0530

    Create  Print Statement.java

---
+++ Practice Challenges/Print Statement.java
@@ -0,0 +1,23 @@
+Given an input, you need to print it and move the cursor to the next line.
+
+Example 1:
+
+Input:
+myInput = "Hello World"
+Output: 
+Hello World
+Explanation: The input is "Hello World",
+and we just print it.
+
+Your Task: 
+Just write the syntax to print the variable that stores the input. The input code is already provided.
+
+Solution:
+public static void utility(String MyInput){
+    //Write your code below that prints myInput
+
+
+    //Write your code below that prints myInput
+    
+    System.out.println(MyInput);
+}
