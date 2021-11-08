e0267a11d0c4243a432a216241398fe715d574d2
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-08 20:37:32 +0530

    Create Pattern searching.java

---
+++ Practice Challenges/Pattern searching.java
@@ -0,0 +1,9 @@
+public class Main
+{
+	public static void main(String[] args) {
+		String s1="geeks for geeks";
+		String s2="geeks";
+		System.out.print(s1.indexOf(s2)+" ");
+		System.out.println(s1.indexOf(s2,1));
+	}
+}
