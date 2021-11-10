dc6fd983651bc32db9169d73f5811377a7fe007c
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-10 10:53:19 +0530

    Create Arraylist add method.java

---
+++ Practice Challenges/Arraylist add method.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<String> al=new ArrayList<String>();
+		al.add("geeks");
+		al.add("ide");
+		al.add("Courses");
+		System.out.println(al);
+		al.add(1,"Practice");
+		System.out.println(al);
+	}
+}
