5f4c2bd1c3823df1626cec823600934e0a3f5238
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-25 16:23:25 +0530

    Create Runnable Interface.java

---
+++ Runnable Interface.java
@@ -0,0 +1,26 @@
+class Test implements Runnable
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"Hello");
+            i++;
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	Test t=new Test();
+	Thread th=new Thread(t);
+	th.start();
+	int i=1;
+	while(true)
+	{
+	    System.out.println(i+"world");
+	    i++;
+	}
+}
+}
