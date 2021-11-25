37f400273d6c4bccca2e393782c2be99190fc6c0
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-11-25 13:03:32 +0530

    Create MultiThreading using Thread class.java

---
+++ MultiThreading using Thread class.java
@@ -0,0 +1,24 @@
+class Test extends Thread
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"Hello");
+        }
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+		Test t=new Test();
+		t.start();
+		int i=1;
+		while(true)
+		{
+		    System.out.println(i+"world");
+		    i++;
+		}
+	}
+}
