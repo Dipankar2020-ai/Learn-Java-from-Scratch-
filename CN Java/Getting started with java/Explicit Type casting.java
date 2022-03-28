0a4bf03fb64743f801dbc76feba4993edeaa0c5d
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-28 11:23:34 +0530

    Create Explicit Type casting.java

---
+++ CN Java/Getting started with java/Explicit Type casting.java
@@ -0,0 +1,19 @@
+public class Explicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long l=100;
+		int i=(int)l;
+		System.out.println(i);
+		System.out.println(l);
+		long l1=12345643218888999L;
+		int i1=(int)l1;
+		System.out.println(l1);
+		System.out.println(i1);
+		
+		
+		
+
+	}
+
+}
