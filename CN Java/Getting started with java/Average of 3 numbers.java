2b2fed47229bca317029b655ff0cfe8d430bad2b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-03-28 09:42:54 +0530

    Create Average of 3 numbers.java

---
+++ CN Java/Getting started with java/Average of 3 numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		String name=sc.next();
+		int ans=(a+b+c)/3;
+		System.out.println(name+"gets "+ans+" marks out of 100");
+		
+		
+
+	}
+
+}
